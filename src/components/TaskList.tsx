c2e66039bf28eb3a92066c000421e701a68b1c2f
Rodrigo Souza <rtz@certi.org.br>
2021-03-09 00:06:53 -0300

    feat: ensure TaskList.handleToggleTaskCompletion complete one task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -29,7 +29,13 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = true;
+      }
+
+      return task;
+    }));
   }
 
   function handleRemoveTask(id: number) {
